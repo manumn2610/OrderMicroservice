87c1c169ceac5f1ceed0307675f2e21ea55e8135
manumn <manumn@localhost>
2022-10-26 09:05:11 -0400

    Added for logging

--- src/main/java/com/javaMicroServices/os/api/controller/OrderController.java
+++ src/main/java/com/javaMicroServices/os/api/controller/OrderController.java
@@ -1,6 +1,7 @@
 package com.javaMicroServices.os.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,24 +13,40 @@ import com.javaMicroServices.os.api.entity.Order;
 import com.javaMicroServices.os.api.entity.TransactionRequest;
 import com.javaMicroServices.os.api.entity.TransactionResponse;
 import com.javaMicroServices.os.api.service.OrderService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @RestController
 @RequestMapping("/order")
 public class OrderController {
 	@Autowired
 	private OrderService service;
-	
+	private static final Logger logger=LogManager.getLogger(OrderController.class);
 
 
 	@PostMapping("/bookOrder")
 	public TransactionResponse bookOrder(@RequestBody TransactionRequest request) {
-		
+		System.out.println("Inside");
 		
 		return service.save(request);
 		
 		//call rest ap to save the payment
 		
 		
+	}
+	
+	@GetMapping("/test")
+	public String tested() {
+		
+		 
+		logger.info("Insider the loggger that i have writen");
+		System.out.println("Insidezzzzz");
+		
+		//return service.save(request);
+		
+		//call rest ap to save the payment
+		
+		return "Helloworld";
 	}
 
 }
